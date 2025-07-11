0f5fc4b962603224d9dd8b0d0812dc5b81beeed6
Steven Fawcett <steven.fawcett@me.com>
2025-07-11 11:19:16 +0100

    Improve status output and introduce startup controller

--- controllers/StatusController.cs
+++ controllers/StatusController.cs
@@ -1,37 +1,41 @@
 ﻿using Microsoft.AspNetCore.Mvc;
-using SimListener;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Reflection;
+using System.Security.Policy;
 
 namespace Broadcast.controllers
 {
+    public class StatusDetails
+    {
+        public bool simulator {  get; set; }
+        public bool redis { get; set; }
+        public string? version { get; set; }
+    }
+
     [Route("api/[controller]")]
     [ApiController]
     public class StatusController : ControllerBase
     {
         [HttpGet]
-        public ActionResult<Dictionary<string, bool>> Get()
+        public ActionResult<StatusDetails> Get()
         {
             // Ensure Program.MainForm and simConnection are not null before accessing them.
             if (Program.MainForm?.simConnection == null)
             {
-                return BadRequest(new Dictionary<string,string>
+                return BadRequest(new Dictionary<string, string>
                         {
                             { "error", "Simulator connection is not initialized." }
                         });
             }
 
-            Dictionary<string, bool> text = new()
-                    {
-                        { "simulator", Program.MainForm.simConnection.isConnected },
-                        { "redis"    , Program.MainForm.redisConnection.isConnected }
-                    };
+            StatusDetails status = new()
+            {
+                version = Assembly.GetExecutingAssembly()?.GetName()?.Version?.ToString() ?? "Unknown",
+                simulator = Program.MainForm.simConnection.isConnected,
+                redis = Program.MainForm.redisConnection.isConnected
+            };
 
             Program.MainForm?.Invoke(new Action(() => { }));
-            return text;
+            return status;
         }
     }
 }
