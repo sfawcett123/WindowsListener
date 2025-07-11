7332a7b4f0bd48e33e7b67f3f6dc101017520e5e
Steven Fawcett <steven.fawcett@me.com>
2025-07-11 10:46:47 +0100

    Set to 0.0.0.0

--- Program.cs
+++ Program.cs
@@ -20,6 +20,7 @@ namespace Broadcast
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
+                .UseUrls("http://0.0.0.0:5000")
                 .UseStartup<Startup>();
     }
 }
